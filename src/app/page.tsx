7a20d3645b96b3761befce1091501b4bd84ae083
yusuf <yusufpazuruan@gmail.com>
2025-01-02 15:46:17 +0700

    ubah text di home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>ini adalah contoh web e-learning</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
